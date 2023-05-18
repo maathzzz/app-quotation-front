2ce122eca708b9b9a8a78004f09f19ec48f620d2
Matheus <matheusdeamorim20@gmail.com>
2023-05-18 12:15:24 -0300

    feat: new image

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,4 +1,5 @@
 import styles from './Banner.module.css'
+import wallet from '../../../public/images/zuvia-wallet.png'
 
 export function Banner() {
   return (
@@ -9,7 +10,15 @@ export function Banner() {
             <h1 className={styles.zuviah1}>Zuvia </h1>
             <h1 className={styles.cotacaoh1}> Cotação </h1>
           </div>
+          <div>
+            <div className={styles.slogan1}>
+                <p className={styles.acompanheMercado}>  Acompanhe o mercado: </p> 
+                <p>Esteja atualizado sobre as últimas variações </p>
+            </div>
+            <p>e tendências do mercado de criptomoedas.</p>
+          </div>
         </div>
+        <img src={wallet} />
     </div>
   )
 }
