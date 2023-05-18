80caedba364660026b182299604783627448f8d3
Matheus <matheusdeamorim20@gmail.com>
2023-05-18 14:36:06 -0300

    fix: resolution

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,21 +4,23 @@ import wallet from '../../../public/images/zuvia-wallet.png'
 export function Banner() {
   return (
     <div className={styles.banner}>
-        <div className={styles.heading}>
-          <span> Cotação em tempo real</span>
-          <div className={styles.title}>
-            <h1 className={styles.zuviah1}>Zuvia </h1>
-            <h1 className={styles.cotacaoh1}> Cotação </h1>
-          </div>
-          <div>
-            <div className={styles.slogan1}>
-                <p className={styles.acompanheMercado}>  Acompanhe o mercado: </p> 
-                <p>Esteja atualizado sobre as últimas variações </p>
+        <div className={styles.content}>
+          <div className={styles.heading}>
+            <span> Cotação em tempo real</span>
+            <div className={styles.title}>
+              <h1 className={styles.zuviah1}>Zuvia </h1>
+              <h1 className={styles.cotacaoh1}> Cotação </h1>
             </div>
-            <p>e tendências do mercado de criptomoedas.</p>
+            <div>
+              <div className={styles.slogan1}>
+                  <p className={styles.p1}>  Acompanhe o mercado: </p> 
+                  <p className={styles.p2}>Esteja atualizado sobre as últimas variações e </p>
+              </div>
+              <p className={styles.p3}>tendências do mercado de criptomoedas.</p>
+            </div>
           </div>
-        </div>
-        <img src={wallet} />
+          <img src={wallet} className={styles.wallet}/>
+      </div>
     </div>
   )
 }
