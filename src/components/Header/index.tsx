32b2e463b81df341c386302209d2f1ee73400117
Matheus <matheusdeamorim20@gmail.com>
2023-05-26 09:24:10 -0300

    fixing deploy

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,17 +5,16 @@ import LogoZuviaDark from '../../../public/images/logoZuviaDark.png'
 export function Header() {
   return (
     <header className={styles.header}>
-        <img src={LogoZuviaDark} alt="Logotipo Zuvia"/>
+        <a href="/" target='__blank'>
+          <img src={LogoZuviaDark} alt="Logotipo Zuvia"/>
+        </a>
         <div className={styles.options}>
-          <a href="https://zuvia.com.br/pay/" target='__blank'> Home </a>
-          <a href="https://zuvia.com.br/" target='__blank'> Zuvia </a>
+          <a href="https://zuvia.com.br/pay/" target='__blank'> Zuvia Pay </a>
           <a href="https://zuvia.com.br/" target='__blank'> Sobre </a>
-          <a href="https://zuvia.com.br/pay/" target='__blank'> Pay </a>
         </div>
-
-            <a href="https://app.zuviapay.com.br/" className={styles.buyCripto} target='__blank'> 
-              Compre Cripto
-            </a>
+        <a href="https://app.zuviapay.com.br/" className={styles.buyCripto} target='__blank'> 
+          Compre Cripto
+        </a>
     </header>
   )
 }
